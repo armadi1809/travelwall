ff2d1ba7ac101ff3476d9b3ece7a313e725b1c9d
Aziz Rmadi <azizrmadi@gmail.com>
2026-02-13 16:07:01 +0100

    properly handle sign out

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import { authClient } from "@/utils/auth-client";
-import { useEffect } from "react";
 
 export const LoginButton = () => {
   const { data } = authClient.useSession();
   const loggedIn = Boolean(data?.session);
 
-  const handleLogin = () => {
-    authClient.signIn.social({ provider: "google" });
+  const handleButtonClick = () => {
+    if (loggedIn) {
+      authClient.signOut();
+    } else {
+      authClient.signIn.social({ provider: "google" });
+    }
   };
+
   return (
     <button
       className="rounded-full border border-slate-700/70 bg-slate-900/60 px-3 py-1 text-sm text-slate-200 shadow-sm cursor-pointer hover:opacity-80 transition"
-      onClick={() => handleLogin()}
+      onClick={handleButtonClick}
     >
       {loggedIn ? "Sign out" : "Sign in with Google"}
     </button>
